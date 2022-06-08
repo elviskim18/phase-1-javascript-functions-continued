180dd767e78fc4139295af36a881a9dac3c1bfbc
Kimani <elvis.kimani@student.moringaschool.com>
2022-06-08 16:47:58 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun(string){
+    if(string == null){
+        return `This Saturday, I want to roller-skate!`
+    }
+    else{
+        return `This Saturday, I want to ${string}!`
+    }
+}
+
+function mondayWork(string){
+    if(string == null){
+        return `This Monday, I will go to the office.`
+    }
+    else{
+        return `This Monday, I will ${string}.`
+    }
+}
+function wrapAdjective(string = "*"){
+   return function(adj = "special"){
+       return `You are ${string}${adj}${string}!`
+   }
+     
+}
